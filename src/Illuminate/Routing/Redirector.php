eb3398030e7745308e23f6c2c4b138beb9218acd
Daniel Gelling <info@danielgelling.nl>
2016-11-16 14:20:52 +0100

    Removed helper file and created Redirector

---
+++ src/Illuminate/Routing/Redirector.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Routing;
+
+use Illuminate\Routing\BaseRedirector;
+// use Illuminate\Http\RedirectResponse;
+// use Illuminate\Session\Store as SessionStore;
+
+class Redirector extends BaseRedirector
+{
+}
